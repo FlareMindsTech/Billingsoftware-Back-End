6593d09235da3de64b8421ca79a9f420163fa085
Gokila-M <gokila1305@gmail.com>
2025-07-29 16:20:59 +0530

    .. get

--- index.js
+++ index.js
@@ -25,5 +25,9 @@ app.use(cors());
 app.use('/api/users', userRoutes);
 app.use('/api/bills', billRoutes); 
 
+app.get("/", (req, res) => {
+  res.send("welcome to Billing Software Server"); 
+});
+
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
